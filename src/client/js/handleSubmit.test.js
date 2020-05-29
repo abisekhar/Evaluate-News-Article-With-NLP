4533a0def48870b3a69a66c945e70891d9df8a33
Abilasch Sekhar <abisekhar@gmail.com>
2020-05-29 14:43:22 -0700

    feat: removed describe keyword

--- src/client/js/handleSubmit.test.js
+++ src/client/js/handleSubmit.test.js
@@ -1,12 +1,11 @@
 import { jestHandleSubmit } from './handleSubmit';
 
-describe("jestHandleSubmit function", () => {
-    test("should call jestHandleSubmit", () => {
-        document.body.innerHTML = '<form onsubmit="return Client.handleSubmit(event)">' +
+
+test("should call jestHandleSubmit", () => {
+    document.body.innerHTML = '<form onsubmit="return Client.handleSubmit(event)">' +
         '<input id="url" type="url" name="input" value="https://www.nytimes.com/2020/05/26/us/coronavirus-crime.html" placeholder="Article Url" required>' +
         '<p id="urlValidMessage"></p>' +
         '<input id="submit" type="submit" name="" value="submit" onclick="return Client.handleSubmit(event)" onsubmit="return Client.handleSubmit(event)"></form>' +
         '<div id="results"></div>';
-        expect(jestHandleSubmit()).toBe(true);
-    });
+    expect(jestHandleSubmit()).toBe(true);
 });
--- src/client/js/validateUrl.test.js
+++ src/client/js/validateUrl.test.js
@@ -1,16 +1,15 @@
 import { validateUrl } from './validateUrl';
 
-describe("ValidateUrl function", () => {
-    test("should call validateUrl", () => {
-        document.body.innerHTML = '<p id="urlValidMessage"></p>';
 
-        const inputs = [
-            { url: "https://www.nytimes.com/2020/05/26/us/coronavirus-crime.html" },
-            { url: "https://www.goodnewsnetwork.org/school-bus-drivers-arrange-aerial-tribute-to-grads-in-quarantine/" }
-        ];
+test("should call validateUrl", () => {
+    document.body.innerHTML = '<p id="urlValidMessage"></p>';
 
-        inputs.forEach(input => {
-            expect(validateUrl(input.url)).toBe(true);
-        });
+    const inputs = [
+        { url: "https://www.nytimes.com/2020/05/26/us/coronavirus-crime.html" },
+        { url: "https://www.goodnewsnetwork.org/school-bus-drivers-arrange-aerial-tribute-to-grads-in-quarantine/" }
+    ];
+
+    inputs.forEach(input => {
+        expect(validateUrl(input.url)).toBe(true);
     });
 });
